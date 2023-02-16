8c536372e598ddcb6cf9d90727a676e0f3a01ec0
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-16 14:49:39 +0530

    Update STTService and tests for accepting esop types
    - Tax rates are different based on the type of esop used in sell order

--- src/main/kotlin/com/esop/service/STTService.kt
+++ src/main/kotlin/com/esop/service/STTService.kt
@@ -4,11 +4,20 @@ import kotlin.math.min
 import kotlin.math.round
 
 class STTService {
-    fun computeTax(quantity: Long, price: Long): Long {
-        return when {
-            quantity <= 100 -> min(round(quantity * price * 0.01).toLong(), 20)
-            quantity in 101..50_000 -> min(round(quantity * price * 0.0125).toLong(), 20)
-            else -> round(quantity * price * 0.015).toLong()
+    fun computeTax(esopType: String, quantity: Long, price: Long): Long {
+
+        return if (esopType == "NON_PERFORMANCE") {
+            when {
+                quantity <= 100 -> min(round(quantity * price * 0.01).toLong(), 20)
+                quantity in 101..50_000 -> min(round(quantity * price * 0.0125).toLong(), 20)
+                else -> round(quantity * price * 0.015).toLong()
+            }
+        } else {
+            when {
+                quantity <= 100 -> min(round(quantity * price * 0.02).toLong(), 50)
+                quantity in 101..50_000 -> round(quantity * price * 0.0225).toLong()
+                else -> round(quantity * price * 0.025).toLong()
+            }
         }
     }
 
--- src/test/kotlin/com/esop/service/STTServiceTest.kt
+++ src/test/kotlin/com/esop/service/STTServiceTest.kt
@@ -12,27 +12,53 @@ class STTServiceTest {
         @JvmStatic
         fun getOrderQuantitiesUpto100(): Stream<Arguments> {
             return Stream.of(
-                Arguments.of(100, 100, 20),
-                Arguments.of(10, 10, 1),
-                Arguments.of(54, 21, 11)
+                Arguments.of("NON_PERFORMANCE",100, 100, 20),
+                Arguments.of("NON_PERFORMANCE",10, 10, 1),
+                Arguments.of("NON_PERFORMANCE",54, 21, 11)
             )
         }
 
         @JvmStatic
         fun getOrderQuantitiesMoreThan100LessThan50k(): Stream<Arguments> {
             return Stream.of(
-                Arguments.of(101, 100, 20),
-                Arguments.of(1001, 1, 13),
-                Arguments.of(540, 3, 20)
+                Arguments.of("NON_PERFORMANCE",101, 100, 20),
+                Arguments.of("NON_PERFORMANCE",1001, 1, 13),
+                Arguments.of("NON_PERFORMANCE",540, 3, 20)
             )
         }
 
         @JvmStatic
         fun getOrderQuantitiesMoreThan50k(): Stream<Arguments> {
             return Stream.of(
-                Arguments.of(50_001, 100, 75002),
-                Arguments.of(100_000, 100_000, 150000000),
-                Arguments.of(54_000, 3, 2430)
+                Arguments.of("NON_PERFORMANCE",50_001, 100, 75002),
+                Arguments.of("NON_PERFORMANCE",100_000, 100_000, 150000000),
+                Arguments.of("NON_PERFORMANCE",54_000, 3, 2430)
+            )
+        }
+
+        @JvmStatic
+        fun getPerformanceOrderQuantitiesUpto100(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of("PERFORMANCE",100, 100, 50),
+                Arguments.of("PERFORMANCE",10, 10, 2),
+                Arguments.of("PERFORMANCE",54, 21, 23)
+            )
+        }
+
+        @JvmStatic
+        fun getPerformanceOrderQuantitiesMoreThan100LessThan50k(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of("PERFORMANCE",101, 100, 227),
+                Arguments.of("PERFORMANCE",1001, 1, 23),
+                Arguments.of("PERFORMANCE",540, 3, 36)
+            )
+        }
+        @JvmStatic
+        fun getPerformanceOrderQuantitiesMoreThan50k(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of("PERFORMANCE",50_001, 100, 125002),
+                Arguments.of("PERFORMANCE",100_000, 100_000, 250000000),
+                Arguments.of("PERFORMANCE",54_000, 3, 4050)
             )
         }
     }
@@ -40,30 +66,62 @@ class STTServiceTest {
     @ParameterizedTest
     @MethodSource("getOrderQuantitiesUpto100")
     fun `It should calculate fee for sell order given order quantity is upto 100`(
-        quantity: Long,
+        esopType: String, quantity: Long,
         price: Long,
         expectedTax: Long
     ) {
-        assertEquals(expectedTax, STTService().computeTax(quantity, price))
+        assertEquals(expectedTax, STTService().computeTax(esopType, quantity, price))
     }
 
     @ParameterizedTest
     @MethodSource("getOrderQuantitiesMoreThan100LessThan50k")
     fun `It should calculate fee for sell order given order quantity in range 101-50k`(
-        quantity: Long,
+        esopType: String, quantity: Long,
         price: Long,
         expectedTax: Long
     ) {
-        assertEquals(expectedTax, STTService().computeTax(quantity, price))
+        assertEquals(expectedTax, STTService().computeTax(esopType, quantity, price))
     }
 
     @ParameterizedTest
     @MethodSource("getOrderQuantitiesMoreThan50k")
     fun `It should calculate fee for sell order given order quantity is more than 50k`(
-        quantity: Long,
+        esopType: String, quantity: Long,
         price: Long,
         expectedTax: Long
     ) {
-        assertEquals(expectedTax, STTService().computeTax(quantity, price))
+        assertEquals(expectedTax, STTService().computeTax(esopType, quantity, price))
+    }
+
+
+
+    @ParameterizedTest
+    @MethodSource("getPerformanceOrderQuantitiesUpto100")
+    fun `It should calculate fee for performance sell order given order quantity is upto 100`(
+        esopType: String, quantity: Long,
+        price: Long,
+        expectedTax: Long
+    ) {
+        assertEquals(expectedTax, STTService().computeTax(esopType, quantity, price))
+    }
+
+    @ParameterizedTest
+    @MethodSource("getPerformanceOrderQuantitiesMoreThan100LessThan50k")
+    fun `It should calculate fee for performance sell order given order quantity in range 101-50k`(
+        esopType: String, quantity: Long,
+        price: Long,
+        expectedTax: Long
+    ) {
+        assertEquals(expectedTax, STTService().computeTax(esopType, quantity, price))
+    }
+
+    @ParameterizedTest
+    @MethodSource("getPerformanceOrderQuantitiesMoreThan50k")
+    fun `It should calculate fee for performance sell order given order quantity is more than 50k`(
+        esopType: String, quantity: Long,
+        price: Long,
+        expectedTax: Long
+    ) {
+        assertEquals(expectedTax, STTService().computeTax(esopType, quantity, price))
     }
 }

093a94a45f010b46c4cf55b7fcf14aeaf2a08371
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-16 15:32:45 +0530

    Extract tax calculation block to functions based on esop types

--- src/main/kotlin/com/esop/service/STTService.kt
+++ src/main/kotlin/com/esop/service/STTService.kt
@@ -9,18 +9,22 @@ class STTService {
     fun computeTax(esopType: String, quantity: Long, price: Long): Long {
         validateArguments(quantity, price, esopType)
         return if (esopType == "NON_PERFORMANCE") {
-            when {
-                quantity <= 100 -> min(round(quantity * price * 0.01).toLong(), 20)
-                quantity in 101..50_000 -> min(round(quantity * price * 0.0125).toLong(), 20)
-                else -> round(quantity * price * 0.015).toLong()
-            }
+            calculateTaxForNonPerformanceESOP(quantity, price)
         } else {
-            when {
-                quantity <= 100 -> min(round(quantity * price * 0.02).toLong(), 50)
-                quantity in 101..50_000 -> round(quantity * price * 0.0225).toLong()
-                else -> round(quantity * price * 0.025).toLong()
-            }
+            calculateTaxForPerformanceESOP(quantity, price)
         }
+    }
+
+    private fun calculateTaxForPerformanceESOP(quantity: Long, price: Long) = when {
+        quantity <= 100 -> min(round(quantity * price * 0.02).toLong(), 50)
+        quantity in 101..50_000 -> round(quantity * price * 0.0225).toLong()
+        else -> round(quantity * price * 0.025).toLong()
+    }
+
+    private fun calculateTaxForNonPerformanceESOP(quantity: Long, price: Long) = when {
+        quantity <= 100 -> min(round(quantity * price * 0.01).toLong(), 20)
+        quantity in 101..50_000 -> min(round(quantity * price * 0.0125).toLong(), 20)
+        else -> round(quantity * price * 0.015).toLong()
     }
 
     private fun validateArguments(quantity: Long, price: Long, esopType: String) {
